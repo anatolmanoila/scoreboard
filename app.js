82e03dba45100335147bcd65a20d0a54df8094ed
Anatolie Manoila <amanoila@deloittece.com>
2019-02-13 18:08:43 +0200

    04 - map over players array

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+const players = [
+    {
+        name: "Michael",
+        score: 50,
+        id: 1
+    },
+    {
+        name: "Andreea",
+        score: 85,
+        id: 2
+    },
+    {
+        name: "John",
+        score: 95,
+        id: 3
+    },
+    {
+        name: "Gabi",
+        score: 80,
+        id: 4
+    }
+];
+
 const Header = (props) => {
     return (
         <header>
@@ -32,25 +55,29 @@ const Player = (props) => {
 
 //App top -level -component
 //Components =  parent - child relationship
-const App = () => {
+const App = (props) => {
     return (
         <div className="scoreboard">
-            <Header title="Scoreboard" totalPlayers={ 1 } />
+            <Header title="Scoreboard" totalPlayers={ props.initialPlayers.length } />
 
             {/* Players list */}
             {/* concept of: independent, self-contained & reusable components - which supports the idea of separation of concerns  */}
             {/* each component in your UI should be responsible for ONE-THING only */}
             {/* the values for these props could come dynamically from a DB or json API */}
-            <Player name="Aema" score={ 29 } />
-            <Player name="Mario" score={ 25 } />
-            <Player name="Andreea" score={ 31 } />
-            <Player name="Michael" score={ 44 } />
-            <Player name="John" score={ 23 } />
+            {/*  keys needs to be unique -- in order for React to keep track of elements that have changed  */}
+            {/* key is needed only for those elements that gets removed or updated or rearranged within UI - so which items where changed, removed or updated from the DOM  */}
+
+            { props.initialPlayers.map( (player) => {
+                return <Player
+                        name={player.name}
+                        score={ player.score }
+                        key={player.id.toString() } />
+            })}
         </div>
     );
 };
 
 ReactDOM.render(
-    <App />,
+    <App initialPlayers={ players } />,
     document.getElementById('root')
 );
