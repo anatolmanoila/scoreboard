d78cc23a5a9d24ade827b198f89144d8a14cc6cf
Anatolie Manoila <amanoila@deloittece.com>
2019-02-19 10:25:05 +0200

    complete Stopwatch

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
-
+import Stopwatch from "./Stopwatch";
 
 const Header = (props) => {
     return (
         <header>
             <h1>{ props.title }</h1>
             <span className="stats">Players: {props.totalPlayers}</span>
+
+            <Stopwatch />
         </header>
+
     );
 };
 
