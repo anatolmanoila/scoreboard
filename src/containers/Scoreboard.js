66ca94e245ecd12a52df7152c2ecc3b21dab38f2
Anatolie Manoila <amanoila@deloittece.com>
2019-02-28 18:17:15 +0200

    restructure Scoreboard component for redux

--- src/containers/Scoreboard.js
+++ src/containers/Scoreboard.js
@@ -1,80 +1,56 @@
-import React, { Component } from 'react'; //named import
+import React, { Component, PropTypes } from 'react'; //named import
+import { bindActionCreators } from 'redux';
+import * as PlayerActionCreators from '../actions/player';
+import { connect } from 'react-redux';
 import './../App.css';
 import Header from "../components/Header";
 import Player from "../components/Player";
 import AddPlayerForm from "../components/AddPlayerForm";
 
 class Scoreboard extends Component {
-  state = {
-    players: [
 
-    ]
-  };
-
-  handleRemovePlayer = (id) => {
-    this.setState(prevState => {
-      return {
-        players: prevState.players.filter(p => { return p.id !== id })
-      };
-    })
-  };
-
-  //new initial player id
-  prevPlayerId = 6;
-
-  //merge the existing obj into current state with the new array being create below
-  handleAddPlayer = (name) => {
-    this.setState( prevState => ({
-      players: [
-        ...prevState.players,
-        {
-        name: name,
-        score: 0,
-        id: this.prevPlayerId += 1
-      }]
-    }))
-  };
-
-
-  handleScoreChange = (index, delta) => {
-      this.setState(prevState => {
-          return {
-            score: prevState.players[index].score += delta
-          };
-      })
-  };
+    static propTypes = {
+        players: PropTypes.array.isRequired
+    };
 
   render() {
-    return (
-      <div className="scoreboard">
 
-        {/* pass the entire state 'players'  to the Header comp */}
+    const { dispatch, players } = this.props;
+    const addPlayer = bindActionCreators(PlayerActionCreators.addPlayer, dispatch);
+    const removePlayer = bindActionCreators(PlayerActionCreators.removePlayer, dispatch);
+    const updatePlayerScore = bindActionCreators(PlayerActionCreators.updatePlayerScore, dispatch);
 
-        <Header title="Scoreboard" players={ this.state.players } />
-
-        {/* Players list */}
-        {/* concept of: independent, self-contained & reusable components - which supports the idea of separation of concerns  */}
-        {/* each component in your UI should be responsible for ONE-THING only */}
-        {/* the values for these props could come dynamically from a DB or json API */}
-        {/*  keys needs to be unique -- in order for React to keep track of elements that have changed  */}
-        {/* key is needed only for those elements that gets removed or updated or rearranged within UI - so which items where changed, removed or updated from the DOM  */}
-
-        {this.state.players.map((player, index) => {
-          return <Player
+    const playerComponents = players.map((player, index) => (
+        <Player
+            index={index}
             name={player.name}
             score={player.score}
             id={player.id}
-            key={player.id.toString()}
-            index={index}
-            removePlayer={this.handleRemovePlayer}
-            changeScore={this.handleScoreChange} />
-        })}
-
-        <AddPlayerForm addPlayer={this.handleAddPlayer} />
-
+            key={player.name}
+            updatePlayerScore={updatePlayerScore}
+            removePlayer={removePlayer}
+        />
+    ));
+    return (
+      <div className="scoreboard">
+        <Header title="Scoreboard" players={ players } />
+        {/* Players list */}
+        {/* concept of: independent, self-contained & reusable components - which supports the idea of separation of concerns  */}
+        {/* each component in your UI should be responsible for ONE-THING ONLY */}
+        {/* the values for these props could come dynamically from a DB or json API */}
+        {/* key is needed only for those elements that gets removed or updated or rearranged within UI - so which items where changed, removed or updated from the DOM  */}
+        {/*  keys needs to be unique -- in order for React to keep track of elements that have changed  */}
+        <div className="players">
+            { playerComponents }
+        </div>
+        <AddPlayerForm addPlayer={ addPlayer } />
       </div>
     );
   }
 }
 
-export default Scoreboard;
+const mapStateToProps = state => ({
+    players: state
+});
+
+export default connect(mapStateToProps) (Scoreboard);
